361352d11ce401e93ed26dc138cbebaf5bc5f44b
Camila Balat <camila.balat@cejam.org.br>
2019-01-16 16:37:33 -0200

    Atualizações

--- app/src/main/java/br/com/camilabalat/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/camilabalat/agenda/ListaAlunosActivity.java
@@ -10,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -18,10 +19,14 @@ import br.com.camilabalat.agenda.modelo.Aluno;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
+    private ListView listaAlunos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        listaAlunos = (ListView) findViewById(R.id.lista_alunos);
 
         Button novoAluno = (Button) findViewById(R.id.novo_alunos);
 
@@ -32,6 +37,8 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 startActivity(intentVaiProFormulario);
             }
         });
+
+        registerForContextMenu(listaAlunos);
     }
 
     @Override
@@ -40,12 +47,24 @@ public class ListaAlunosActivity extends AppCompatActivity {
         carregaLista();
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem deletar = menu.add("Deletar");
+        deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(ListaAlunosActivity.this, "Clicou em deletar!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+
     public void carregaLista(){
         AlunoDAO dao = new AlunoDAO(this);
         List<Aluno> alunos = dao.buscaAlunos();
         dao.close();
 
-        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+//        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
         ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this,  android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
     }
